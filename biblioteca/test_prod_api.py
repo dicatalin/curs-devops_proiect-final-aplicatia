f612bfdfee277c171e2d091f373d3d1ce8302e4b
Catalin Dumitru <catalin.dumitru@profi.ro>
2026-03-23 16:54:50 +0200

    modificat logica smoke tests

--- biblioteca/test_prod_api.py
+++ biblioteca/test_prod_api.py
@@ -1,22 +1,31 @@
+import os
 import unittest
 import requests
-# Importăm asta pentru a scăpa de mesajele de avertizare (InsecureRequestWarning) din consolă
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class StagingLiveTest(unittest.TestCase):
-    URL = "https://prod-host.home.local/"
+    def setUp(self):
+        # Citim variabilele trimise de Jenkins
+        # Dacă rulăm local și nu le avem, punem niște default-uri
+        self.host = os.getenv("TEST_HOST", "192.168.122.76")
+        self.port = os.getenv("TEST_PORT", "8081") # default blue
+        
+        # Construim URL-ul către instanța specifică (Green)
+        # Atenție: Folosim IP:Port pentru a ocoli Load Balancer-ul/Nginx dacă e nevoie
+        self.url = f"http://{self.host}:{self.port}/"
 
     def test_api_and_ui_on_live_server(self):
-        # Folosim verify=False pentru a ignora certificatul auto-semnat
-        response = requests.get(self.URL, verify=False)
+        # Folosim Header-ul de 'Host' pentru ca Nginx/Django să știe ce site cerem
+        # chiar dacă mergem direct pe IP și Port
+        headers = {'Host': 'prod-host.home.local'}
         
-        print(f"\n[DEMO] Testare mediu LIVE la: {self.URL}")
-        print(f"[DEMO] Status răspuns: {response.status_code}")
+        print(f"\n[SMOKE TEST] Testare mediu NOU (GREEN) la: {self.url}")
+        response = requests.get(self.url, headers=headers, verify=False)
         
-        # Verificăm dacă serverul e Online
-        self.assertEqual(response.status_code, 200, "Serverul de Staging nu răspunde!")
+        print(f"[SMOKE TEST] Status răspuns: {response.status_code}")
         
-        # UI Test: Verificăm dacă în HTML-ul primit există cuvinte cheie
-        self.assertIn("Titlu", response.text, "Câmpul 'Titlu' nu a fost găsit în interfață!")
-        self.assertIn("Autor", response.text, "Câmpul 'Autor' nu a fost găsit în interfață!")
+        self.assertEqual(response.status_code, 200, f"Mediul Green ({self.url}) nu răspunde!")
+        self.assertIn("Titlu", response.text)
+        self.assertIn("Autor", response.text)
